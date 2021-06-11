bd1fb438771ebb698a0cdcf6e6d75ad27cbf4dff
Esau Sanchez <tonalfan@gmail.com>
2021-06-11 01:56:53 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,43 @@
+//Servidor de Express
+const express = require('express');
+//Servidor de sockets
+const http    = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server {
+    constructor() {
+        this.app = express();;
+        this.port = process.env.PORT;
+        
+        // Http server
+        this.server = http.createServer(this.app);
+
+        //configuraciones de sockets
+        this.io = socketio(this.server);
+    }
+
+    middlewares() {
+        //desplegar el directorio publico
+        this.app.use(express.static( path.resolve(__dirname, '../public') ));
+    }
+
+    configurarSockets(){
+        new Sockets(this.io);
+    }
+
+    execute() {
+        //Inicializar middlewares();
+        this.middlewares();
+        //Inicializar sockets
+        this.configurarSockets();
+        //Inicializar Server
+        this.server.listen(this.port, () => {
+            console.log('Server en puerto: ', this.port);
+        });
+    }
+
+}
+
+module.exports = Server;
